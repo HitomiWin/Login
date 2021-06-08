c0c0e1bfa91dcc99eab95177d09827542dacba19
Hitomi <hitomi.winberg@gmail.com>
2021-06-08 20:52:35 +0200

    Add css to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom"
+import styles from "../css/Navbar.module.css"
 
 export default function Navbar() {
-  return (
-    
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <div>Login</div>
+  return ( 
+    <nav className={styles.navbar}>
+      <div>Logout</div>
     </nav>
   )
 }
