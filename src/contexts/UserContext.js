9115989eada0a3e9d68ac979ded22bda3587ab2b
Hitomi <hitomi.winberg@gmail.com>
2021-06-08 22:46:15 +0200

    Fixed value at UserContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,6 +4,10 @@ export const UserContext = createContext();
 
 export default function UserContextProvider (props){
 
+  const value={
+    
+  }
+
   return(
     <UserContext.Provider value ={value}>
       {props.children}
